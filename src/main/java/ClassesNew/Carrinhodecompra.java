f861be732778870a94b2501dabc348919d90f21d
Julia Milioranza Gomes <julia.meg1990@gmail.com>
2023-09-23 17:31:53 -0400

    ADD: classe Carrinho de compras

---
+++ src/main/java/ClassesNew/Carrinhodecompra.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClassesNew;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Julia
+ */
+public class Carrinhodecompra {
+    private List<Produto> itens;
+
+    public Carrinhodecompra() {
+        this.itens = new ArrayList<>();
+    }
+}
