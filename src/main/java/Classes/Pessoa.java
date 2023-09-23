3f9af09e6cefb70f324ee1e043b419dc733a3d4c
tuliana <76637285+naoentendotu@users.noreply.github.com>
2023-09-23 17:04:02 -0400

    Corrigido

--- src/main/java/Classes/Pessoa.java
+++ src/main/java/Classes/Pessoa.java
@@ -2,62 +2,6 @@
 package Classes;
 
 public class Pessoa {
-    private int id_animal;
-    private String nome;
-    private String especie;
-    private String raça;
-    private int idade;
-    private String sexo;
-
-    public int getId_animal() {
-        return id_animal;
-    }
-
-    public void setId_animal(int id_animal) {
-        this.id_animal = id_animal;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEspecie() {
-        return especie;
-    }
-
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-
-    public String getRaça() {
-        return raça;
-    }
-
-    public void setRaça(String raça) {
-        this.raça = raça;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-    
-    
     protected int ID_pessoa;
     protected String nome;
     protected int telefone;
