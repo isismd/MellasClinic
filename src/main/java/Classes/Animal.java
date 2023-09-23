54ce23f3975c6985c0c930a5749f582357433ee8
Julia Milioranza Gomes <julia.meg1990@gmail.com>
2023-09-23 16:57:52 -0400

    ADD: classe animal

--- src/main/java/Classes/Animal.java
+++ src/main/java/Classes/Animal.java
@@ -1,7 +1,14 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Classes;
 
-public class Pessoa {
+/**
+ *
+ * @author Julia
+ */
+public class Animal {
     private int id_animal;
     private String nome;
     private String especie;
@@ -56,6 +63,4 @@ public class Pessoa {
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
-    
-    
 }
