c96d190814faf4f6b0e3e1a8c63a3c240f22a967
isism <daronisism@gmail.com>
2023-10-04 15:17:16 -0400

    fix: cliente

--- src/main/java/ClassesNew/Cliente.java
+++ src/main/java/ClassesNew/Cliente.java
@@ -2,7 +2,7 @@ package ClassesNew;
 
 public class Cliente extends Pessoa {
 
-    boolean elegivelDesconto;
+    private boolean elegivelDesconto;
 
     public Cliente(int ID_pessoa, String nome, long telefone, String email, String cidade, String endereco) {
         super(ID_pessoa, nome, telefone, email, cidade, endereco);
